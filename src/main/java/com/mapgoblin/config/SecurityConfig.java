ddc5821c5de02044d24bc7bd12f669db424d677b
Choi-Yong-Yeol <88dydfuf@naver.com>
2021-05-15 22:18:40 +0900

    cors

--- src/main/java/com/mapgoblin/config/SecurityConfig.java
+++ src/main/java/com/mapgoblin/config/SecurityConfig.java
@@ -59,7 +59,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.addAllowedOrigin("http://localhost");
+        configuration.addAllowedOrigin("*");
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");
         configuration.setAllowCredentials(true);
